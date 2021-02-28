cdfc0595216f808e578a5ef2d6d978d5443fb3eb
alex-tenczar <alex-desktop@pop-os.localdomain>
2021-02-28 11:00:57 -0500

    Added Searched Artist Lightbox + styling

--- src/components/LastViz.js
+++ src/components/LastViz.js
@@ -2,97 +2,159 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 import {useD3} from '../hooks/UseD3.js';
 import { Link } from 'react-router-dom';
+import { scaleSqrt } from 'd3';
 
 function LastViz(props) {
-    console.log(props.results);
-    const data = props.results;
+    console.log(props.lastResults);
+    console.log(props.spotResults);
+    const dataa = props.spotResults;
+    //const data = props.lastResults;
+    const data = props.lastResults.map(d => ({
+      ...d,
+      x: 900,
+      y: 900
+    }))
         const ref = useD3(
             (svg) => {
-              const height = 500;
-              const width = 1000;
+              const height = 1500;
+              const width = 1500;
               const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-        
-              const x = d3
-                .scaleBand()
-                .domain(data.map((d) => d.name))
-                .rangeRound([margin.left, width - margin.right])
-                .padding(0.1);
+              const center = { x: width/2, y: height/2 };
+            
+            var distanceScale = d3.scaleSqrt().domain([0, 1]).range([5, 80])
 
-                        
-              const y1 = d3
-                .scaleLinear()
-                .domain([0, d3.max(data, (d) => d.match)])
-                .rangeRound([height - margin.bottom, margin.top]);
-        
-              const xAxis = (g) =>
-                g.attr("transform", `translate(0,${height - margin.bottom})`).call(
-                  d3
-                    .axisBottom(x)
-                    .tickValues(
-                      d3
-                        .ticks(...d3.extent(x.domain()), width / 40)
-                        .filter((v) => x(v) !== undefined)
-                    )
-                    .tickSizeOuter(0)
-                );
-        
-              const y1Axis = (g) =>
-                g
-                  .attr("transform", `translate(${margin.left},0)`)
-                  .style("color", "steelblue")
-                  .call(d3.axisLeft(y1).ticks(null, "s"))
-                  .call((g) => g.select(".domain").remove())
-                  .call((g) =>
-                    g
-                      .append("text")
-                      .attr("x", -margin.left)
-                      .attr("y", 10)
-                      .attr("fill", "currentColor")
-                      .attr("text-anchor", "start")
-                      .text(data.y1)
-                  );
-        
-              svg.select(".x-axis").call(xAxis);
-              svg.select(".y-axis").call(y1Axis);
-                        
-              svg
-                .select(".plot-area")
-                .attr("fill", "steelblue")
-                .selectAll(".bar")
-                .data(data)
-                .join("rect")
-                .attr("class", "bar")
-                .attr("x", (d) => x(d.name))
-                .attr("name", (d) => (d.name))
-                .attr("width", x.bandwidth())
-                .attr("y", (d) => y1(d.match))
-                .attr("height", (d) => y1(0) - y1(d.match))
-                .on("click", function(d){
-                  console.log(d.target.__data__);
-                  const artist = d.target.__data__;
-                  const artist_link = "/search/" + artist.name.replace(/\s/g, '+');
-                  window.location.href = artist_link;
-                });
-            },
-            [data.length]
-          );
+            var defs = svg.append("defs");
+
+            var Tooltip = d3.select("#div_template")
+              .append("div")
+              .style("opacity", 0)
+              .attr("class", "tooltip")
+              .style("background-color", "rgba(38, 38, 38, 0.95)")
+              .style("border-radius", "5px")
+              .style("padding", "10px")
+              .style("position", "absolute")
+              .style("color", "white")
+
+            var mouseover = function(d) {
+              Tooltip
+                .style("opacity", 1)
+              d3.select(this)
+                .style("stroke", "black")
+                .style("opacity", .5)
+                .style("cursor", "pointer")
+            }
+            var mousemove = function(event,d) {
+              Tooltip
+                .html(d.name + "<br>Similarity: " + (d.match * 100).toFixed(2))
+                .style("left", (event.pageX+20) + "px")
+                .style("top", (event.pageY) + "px")
+            }
+            var mouseleave = function(d) {
+              Tooltip
+                .style("opacity", 0)
+              d3.select(this)
+                .style("stroke", "none")
+                .style("opacity", 1)
+            }
+
+            var circles = svg.selectAll(".artist")
+              .data(data)
+              .enter().append("circle")
+              .attr("class", "artist")
+              .attr("r", function(d){
+                return distanceScale(d.match);
+              })
+              .attr("fill", function(d, i) {
+                return "url(#" + d.name.toLowerCase().replace(/[ "']/g, "+") + ")";
+              })
+              .on("mouseover", mouseover)
+              .on("mousemove", mousemove)
+              .on("mouseleave", mouseleave)
+              .on("click", function(d){
+                const artist = d.target.__data__;
+                const artist_link = "/search/" + artist.name.replace(/\s/g, '+');
+                window.location.href = artist_link;
+              })
+              
+
+            defs.selectAll(".artist-pattern")
+              .data(dataa)
+              .enter().append("pattern")
+              .attr("class", "artist-pattern")
+              .attr("id", function(d){
+                return d.name.toLowerCase().replace(/[ "']/g, "+");
+              })
+              .attr("height", "100%")
+              .attr("width", "100%")
+              .attr("patternContentUnits", "objectBoundingBox")
+              .append("image")
+              .attr("height", 1)
+              .attr("width", 1)
+              .attr("preserveAspectRatio", "xMidYMid slice")
+              .attr("object-fit", "cover")
+              .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+              .attr("xlink:href", function(d){
+                var imageUrl = d.images[1]
+                if(imageUrl != null){
+                  return imageUrl.url;
+                }
+              })
+              
+              // charge is dependent on size of the bubble, so bigger towards the middle
+              function charge(d) {
+                return Math.pow(distanceScale(d.match), 2.0) * 0.01
+              }
+
+            var simulation = d3.forceSimulation()
+              .force('charge', d3.forceManyBody().strength(charge))
+              .force("x", d3.forceX(width / 2).strength(0.05).x(center.x))
+              .force("y", d3.forceY(height / 2).strength(0.05).y(center.y))
+              .force("collide", d3.forceCollide(function(d){
+                return distanceScale(d.match);
+              }))
+            
+
+            if(data !== null)
+            {
+              simulation.nodes(data)
+              .on('tick', ticked)
+            }
+
+
+            function ticked() {
+              circles
+                .attr("cx", function(d) {
+                  return d.x
+                })
+                .attr("cy", function(d) {
+                  return d.y
+                })
+            }
+
+        },
+        [data.length]
+      );
 
     
 
         return (
+          <div id="div_template">
             <svg
               ref={ref}
               style={{
-                height: 500,
-                width: "100%",
+                height: (window.innerHeight - 100),
+                width: '100%',  
                 marginRight: "0px",
                 marginLeft: "0px",
               }}
+              viewBox="0 0 1500 1500"
+              preserveAspectRatio="xMidYMid meet"
             >
-              <g className="plot-area" />
-              <g className="x-axis" />
-              <g className="y-axis" />
+              <defs>
+                
+              </defs>
             </svg>
+          </div>
           );
 
 }
--- src/components/SimilarArtists.js
+++ src/components/SimilarArtists.js
@@ -3,8 +3,10 @@ import * as config from '../config/config.js';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import LastViz from './LastViz.js'
+import '../styles/layout/lightbox.scss';
 
-const last_url = 'https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=';
+const last_similar_url = 'https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=';
+const last_search_url = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=';
 const spot_search_url = 'https://api.spotify.com/v1/search?q=';
 const spot_similar_url = 'https://api.spotify.com/v1/artists/'
 const last_key = config.keys.last_api_key;
@@ -15,16 +17,32 @@ class SimilarArtists extends Component {
         super();
         this.state = {
             artist: undefined,
-            artistObject: {},
+            spotArtistObject: {},
+            lastArtistObject: {},
             lastArtists: [],
             spotArtists: [],
+            lastFinalArtists: [],
+            spotFinalArtists: [],
+            lightboxVisibility: false,
         }
     }
 
     async getLast(){
-        const lastPromise = await axios.get(`${last_url}${this.state.artist}&api_key=${last_key}&format=json`)
+        const lastPromise = await axios.get(`${last_similar_url}${this.state.artist}&api_key=${last_key}&format=json`)
         console.log(lastPromise);
         return lastPromise;
+    }
+
+    getLastSearch = () => {
+        const artistName = this.state.artist;
+        console.log(this.state.artist);
+        axios.get(`${last_search_url}${artistName}&api_key=${last_key}&format=json`)
+        .then(({ data }) => {
+            if(typeof data.artist !== "undefined") {
+                this.setState({lastArtistObject: data.artist});
+                console.log(data.artist)
+            }
+        })
     }
 
     getSpotSearch = () => { //used for querying search artist and creating artist object
@@ -41,7 +59,7 @@ class SimilarArtists extends Component {
         })
         .then(({ data }) => {
             if(typeof data.artists !== "undefined" && artistName !== "") {
-                this.setState({artistObject: data.artists.items[0]});
+                this.setState({spotArtistObject: data.artists.items[0]});
                 console.log(data);
             }
         })
@@ -54,8 +72,8 @@ class SimilarArtists extends Component {
         this.getLast().then(({data}) => {
             this.setState({lastArtists: data.similarartists.artist.slice()}, () => {
                 var j = 0;
-                if(this.state.lastArtists.length === 0 && typeof this.state.artistObject !== 'undefined') { //if last.fm didn't return any similar artists
-                    const artist_id = this.state.artistObject.id;
+                if(this.state.lastArtists.length === 0 && typeof this.state.spotArtistObject !== 'undefined') { //if last.fm didn't return any similar artists
+                    const artist_id = this.state.spotArtistObject.id;
                     promises.push(axios.get(`${spot_similar_url}${artist_id}/related-artists`, {
                         headers: {
                             "Accept": "application/json",
@@ -85,11 +103,11 @@ class SimilarArtists extends Component {
                                 tempLastArtistArray.push(similarQuery);
                                 tempSpotArtistArray.push(spotifySearchResult);
                             }
-                            else
+/*                             else
                             {
                                 tempSpotArtistArray.push('null');
                                 tempLastArtistArray.push('null');
-                            }
+                            } */
                             j++;
                         }).catch(err => {
                             return null;
@@ -99,8 +117,8 @@ class SimilarArtists extends Component {
                         console.log(tempSpotArtistArray);
                         console.log(tempLastArtistArray);
                         console.log(promises);
-                        this.setState({spotArtists: tempSpotArtistArray})
-                        this.setState({lastArtists: tempLastArtistArray})
+                        this.setState({spotFinalArtists: tempSpotArtistArray})
+                        this.setState({lastFinalArtists: tempLastArtistArray})
                     });
                 }
             })
@@ -111,6 +129,7 @@ class SimilarArtists extends Component {
         this.setState({artist: this.props.artist}, () =>
         {
             this.getSpotSearch();
+            this.getLastSearch();
             this.getSpotSimilar();
         });
     }
@@ -122,27 +141,61 @@ class SimilarArtists extends Component {
                 this.setState({spotArtists: []})
                 this.setState({lastArtists: []})
                 this.getSpotSearch();
+                this.getLastSearch();
                 this.getSpotSimilar();
             })
         }
     }
+
+    showLightbox = () => {
+        if(this.state.lightboxVisibility == false){
+            this.setState({lightboxVisibility: true});
+        }else{
+            this.setState({lightboxVisibility: false});
+        }
+
+        console.log(this.state.spotArtistObject);
+    }
     
     render() {
         const images = [];
-        const ao = this.state.artistObject;
+        const genres = [];
+        let lightbox;
+        const sao = this.state.spotArtistObject;
+        const lao = this.state.lastArtistObject;
         let lastViz;
         let sourceArtist;
-        if(typeof ao !== "undefined" && typeof ao.images !== "undefined") {
+        if(typeof sao !== "undefined" && typeof sao.images !== "undefined" && typeof lao !== "undefined" && typeof lao.bio !== "undefined") {
+            sao.genres.map((i) => {
+                genres.push(<li>{i}</li>)
+            })
+            let last_link = <a href={lao.url}>Last.fm</a>
+            let spot_link = <a href={sao.external_urls.spotify}>Spotify</a>
             sourceArtist = 
-                <div key={ao.name}> 
-                    <h1>{ao.name}</h1>
-                    <img src={ao.images[1].url}></img>
+                <div key={sao.name}> 
+                    <h1 onClick={this.showLightbox}>{sao.name}</h1>
+                    <div className="lightbox" style={{ visibility: this.state.lightboxVisibility ? "visible" : "hidden", opacity: this.state.lightboxVisibility ? "1" : "0"}}>
+                        <h1 className="lightbox-title">{sao.name}</h1>
+                        <div className="external-links">
+                            {last_link}
+                            {spot_link}
+                        </div>
+                        <div className="lightbox-container">
+                            <div className="lightbox-image" style={{backgroundImage: `url(${sao.images[1].url})`}}></div>
+                                <div className="artist_info">
+                                <ul>                           
+                                    <h2>Genre(s)</h2>
+                                    {genres}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>;
         }
-        if(this.state.spotArtists.length !== 0 && this.state.lastArtists.length !== 0) {    //if last.fm similar artists exist, render them.
+        if(this.state.spotFinalArtists.length !== 0 && this.state.lastFinalArtists.length !== 0) {    //if last.fm similar artists exist, render them.
             for (var k = 0; k < this.state.lastArtists.length; k++) {
-                    const last_results = this.state.lastArtists
-                    const spot_results = this.state.spotArtists
+                    const last_results = this.state.lastFinalArtists
+                    const spot_results = this.state.spotFinalArtists
                     //console.log(this.state.lastArtists);
                     //console.log(this.state.spotArtists);
                     if(typeof spot_results[k] !== "undefined" && typeof spot_results[k].images !== "undefined" && spot_results[k] !== 'null') {
@@ -150,33 +203,23 @@ class SimilarArtists extends Component {
                             const artist_name = last_results[k].name;
                             const artist_link = "/search/" + artist_name.replace(/\s/g, '+');
                             console.log(spot_results.length);
-                            images.push(
-                                <div className="artist-container" key={artist_name}>
-                                    <Link to={{pathname:artist_link, state: spot_results[k]}} className="artist-link" key={artist_name}><img src={spot_results[k].images[2].url} key={spot_results[k].name}></img></Link>
-                                </div>
-                            );
                         }
                     }                
             }
-            lastViz = <LastViz results={this.state.lastArtists}></LastViz>
+            lastViz = <LastViz lastResults={this.state.lastFinalArtists} spotResults={this.state.spotFinalArtists}></LastViz>
         }
         if(this.state.spotArtists.length !== 0 && this.state.lastArtists.length === 0){ //if last.fm similar artists don't exit, render spotify's similar artists.
             for(var l = 0; l < this.state.spotArtists.length; l++) {
                 const spot_results = this.state.spotArtists;
                 const artist_name = spot_results[l].name;
                 const artist_link = "/search/" + artist_name.replace(/\s/g, '+');
-                images.push(
-                    <div className="artist-container" key={artist_name}>
-                        <Link to={{pathname:artist_link, state: spot_results[l]}} className="artist-link" key={artist_name}><img src={spot_results[l].images[2].url} key={spot_results[l].name}></img></Link>
-                    </div>
-                );
             }
         }
 
 
         return (
             <div>
-                {/* <h1>{this.state.artist}</h1> */}
+                {lightbox}
                 {sourceArtist}
                 {lastViz}
                 {images}
